14da367f313fc300d351b16810bab3bbdaa823a4
rafaelmolin <rafaelmolina0101@gmail.com>
2024-06-05 20:54:08 -0500

    ConceptosBasicos js

---
+++ JavaScript/ConceptosBasicos/README.md
@@ -0,0 +1,21 @@
+## TITULO: FUNDAMENTOS DE JAVASCRIPT
+
+## DESCRIPCION
+
+los conceptos basicos que estaremos abordando durante las proximas clases y que implementaremos en nuestro proyecto final son: (conceptos basicos de la programacion)
+
+1. variables y tipos de datos
+2. funciones
+3. control de flujo -> condicionales y ciclos
+
+--------------------------------------------------------------------------
+
+se enfoca en la interactividad del sitio web -> interaccion del navegador
+
+4. manejo de dow -> dinamismo
+5. manejo de eventos 
+6. js asincronico y funciones asinconas -> promesas
+
+## AUTHOR
+
+proyecto realizado por Rafael Molina
---
+++ JavaScript/ConceptosBasicos/scripts/conceptosBasicos.js
@@ -0,0 +1,47 @@
+// 1. lo primero es verificar que la vinculacion es correcta 
+// ENVIAMOS UN MENSAJE 
+
+console.log("HOLA MUNDO - estoy vinculado con mi html");
+
+// VARIABLES DE JAVASRCRIPT
+
+/* DEFINICION - ESPACIO DE MEMORIA - cajita que guarda informacion en forma de datos de diferentes tipos, numerico (2, 2.3, 4.65487465874), boleabnos, arreglos, texto (strings), objetos */
+
+// definimos para usarlos constantemente 
+
+/*
+VARIABLES EN JS
+(no se usa var)
+1. var - nos guarda datos que pueden cambiar en un tiempo (en desuso)
+
+2. let - nos guarda datos que pueden cambiar en un tiempo
+3. const - nos guarda datos constantes - es decir no cambian en el tiempo
+*/
+
+// 2. DECLARACION DE VARIABLES
+/* 
+1. definir que tipo de variable (let o const)
+2. darle nombrfe a la variable
+3. decirle que va a  guardar
+*/
+let nombreVariable = 'nombre';
+
+// numericos
+
+let numero = 3;
+const pi = 3.14;
+
+// texto - strings
+
+let texto = 'hola';
+const nombre = 'rafa';
+
+// booleanos - verdadero o falso
+
+let esVerdadero = true;
+const esFalso = false;
+
+// arreglos - permiten guardar mas de un dato - del mismo tipo
+// se reconocen por uso de corchetes y los datos se separan por comas
+let arregloNumero = [5, 6, 7, 3, 9];
+const arregloNombres = ['luisa', 'manuela', 'daniel'];
